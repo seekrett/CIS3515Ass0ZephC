851ea4562d56f351138860df3a4e4be2c1999fad
Yuto <63772196+seekrett@users.noreply.github.com>
2025-01-21 22:10:59 -0500

    Create Square.kt

---
+++ Square.kt
@@ -0,0 +1,18 @@
+class Square(_name: String) : Shape(_name) {
+
+    private var length : Double = 0.0
+    private var height : Double = 0.0
+
+    fun setDimensions(length : Double, height : Double) {
+        this.length = length
+        this.height = height
+    }
+
+    override fun printDimensions() : String {
+        return "Dimensions: Length [$length] Height [$height]"
+    }
+
+    override fun getArea(): Double {
+        return length * height
+    }
+}
